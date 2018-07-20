365ec8dc2fb2262b965db57e8610c0083052ab08
urosh <urosh.stojkovikj@gmail.com>
2018-07-20 08:10:24 -0700

    Homework W5D5

---
+++ W5D5/homework.js
@@ -0,0 +1,38 @@
+window.setTimeout(function() {
+  alert('HAMMERTIME');
+}, 5000)
+
+
+// --------------------------
+
+function hammerTime (time) {
+  window.setTimeout(function () {
+    alert(`${time} is HAMMER TIME!`);
+  });
+}
+
+// ------------------------------------------
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits () {
+  let first, second;
+
+  reader.question('Would you like some tea?', (res) => {
+    console.log(`You replied ${res}.`);
+    reader.question('Would you like some biscuits?', (res) => {
+      console.log(`You replied ${res}.`);
+
+      const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
+    });
+  });
+}
